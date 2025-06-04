95e2c3d9b7160af4dc264a79aaf8f05158d03c82
Werner Donné <werner.donne@pincette.biz>
2025-06-04 17:05:51 +0200

    Update dependencies

--- src/main/java/net/pincette/http/headers/Application.java
+++ src/main/java/net/pincette/http/headers/Application.java
@@ -15,7 +15,7 @@ import java.util.logging.Logger;
  */
 public class Application {
   static final Logger LOGGER = getLogger("net.pincette.http.headers");
-  private static final String VERSION = "1.1.0";
+  private static final String VERSION = "1.1.1";
 
   @SuppressWarnings("java:S106") // Not logging. Its just a CLI.
   public static void main(final String[] args) {
